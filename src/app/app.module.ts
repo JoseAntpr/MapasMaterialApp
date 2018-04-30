fc7fbdbe2e37ecac25e37ac58d42e50f51b28735
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-04-30 10:58:58 +0200

    Dialog: mostando un modal para la edición del marcador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { MapaComponent } from './components/mapa/mapa.component';
+import { MapaEditarComponent } from './components/mapa/mapa-editar.component';
 
 import { AgmCoreModule } from '@agm/core';
 import { environment } from '../environments/environment';
 
+
 @NgModule({
+  entryComponents: [
+    MapaEditarComponent
+  ],
   declarations: [
     AppComponent,
-    MapaComponent
+    MapaComponent,
+    MapaEditarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Marcador } from '../../classes/marcador.class';
 import { MatSnackBar } from '@angular/material';
+import { MatDialog, MatDialogRef } from '@angular/material';
+import { MapaEditarComponent } from './mapa-editar.component';
 
 @Component({
   selector: 'app-mapa',
@@ -14,7 +16,8 @@ export class MapaComponent implements OnInit {
   lat = 51.678418;
   lng = 7.809007;
 
-  constructor(public snackBar: MatSnackBar) {
+  constructor(public snackBar: MatSnackBar,
+              public dialog: MatDialog) {
     if ( localStorage.getItem('marcadores') ) {
       this.marcadores = JSON.parse(localStorage.getItem('marcadores'));
     }
@@ -35,6 +38,14 @@ export class MapaComponent implements OnInit {
     this.marcadores.splice(id, 1);
     this.guardarStorage();
     this.snackBar.open('Marcador Borrado ', 'Cerrar', {duration: 1000});
+  }
+
+  editarMarcador( marcador: Marcador ) {
+    const dialogRef = this.dialog.open( MapaEditarComponent, {
+      width: '250px',
+      data: { titulo: marcador.titulo, desc: marcador.desc }
+    });
+
   }
 
   guardarStorage() {
